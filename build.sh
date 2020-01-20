d7e73cd76cb833c450c2b5ee6dba287b5c1268a7
josem <starinsun@outlook.com>
2020-01-21 00:40:56 +0800

    1

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 ###
  # @Date: 2020-01-20 21:23:47
  # @LastEditors  : Asen Wang
- # @LastEditTime : 2020-01-21 00:11:18
+ # @LastEditTime : 2020-01-21 00:40:45
  # @content: I
  ###
-docker build -t registry.cn-shanghai.aliyuncs.com/first_ry/fblog:v2 .
+docker build -t registry.cn-shanghai.aliyuncs.com/first_ry/fblog:v3 .
 docker run -d -p 3000:3000 --name blog registry.cn-shanghai.aliyuncs.com/first_ry/fblog:v2
-docker push registry.cn-shanghai.aliyuncs.com/first_ry/fblog:v2
+docker push registry.cn-shanghai.aliyuncs.com/first_ry/fblog:v3
