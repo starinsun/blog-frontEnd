84872b6a721fdc0e1024d89a78d30407b0f8927e
josem <starinsun@outlook.com>
2020-02-18 23:49:21 +0800

    sudo解决ENOTEMPTY: directory not empty

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import App from "next/app";
 import "antd/dist/antd.css";
 import "../utils/np.css";
 import "markdown-navbar/dist/navbar.css";
 import "gitalk/dist/gitalk.css";
 import "highlight.js/styles/monokai-sublime.css";
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
